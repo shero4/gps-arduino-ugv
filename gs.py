466d82f2afeaf22f133c7ee991cefc9cd5f71f7d
Sitaraman <sitaraman.raghav.india@gmail.com>
2021-11-23 11:48:20 +0530

    threading

--- gs.py
+++ gs.py
@@ -1,10 +1,11 @@
 #! /usr/bin/python3
 
+import threading
 import serial
 import time
 import sys
 
-ttyPort = "/dev/" + str(sys.argv[1])
+ttyPort = "/dev" + str(sys.argv[1])
 
 rfd900 = serial.Serial(port=ttyPort, baudrate=57600, timeout=1)
 time.sleep(3)
@@ -21,6 +22,11 @@ def winchDrop():
 def writeRandomData():
     while True:
         rfd900.write(b'A')
+
+def getTelemetry():
+    while True:
+        print(rfd900.readline().decode('utf-8'))
+           
 
 def manualControl(): 
     while True:
@@ -54,7 +60,19 @@ def manualControl():
         elif(command == '9'):
             print("Go to waypoint")
             rfd900.write(bytes('9\n', 'utf-8'))
-            while True:
-                print(rfd900.readline().decode('utf-8'))
             
-manualControl()
+# manualControl()
+t1 = threading.Thread(target=manualControl)
+t2 = threading.Thread(target=getTelemetry)
+
+
+# starting thread 1
+t1.start()
+# starting thread 2
+t2.start()
+
+
+# wait until thread 1 is completely executed
+t1.join()
+# wait until thread 2 is completely executed
+t2.join()
